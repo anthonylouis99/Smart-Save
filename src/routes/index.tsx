922130b71f2a19e8d6c431c734bcf5b019d67ec7
anthonyLouis99 <orjidalu@gmail.com>
2025-10-06 21:34:01 +0100

    Fix 404 three  routing for Vercel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
 
   {
     path: "/",
-    element: <Navigate to="/dashboard" replace />,
+    element: <Navigate to="/signIn" replace />,
   },
   {
     path: "/signIn",
